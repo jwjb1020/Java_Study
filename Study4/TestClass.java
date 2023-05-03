d88dafd865e85dc6cc56682b239cbf366cab4fa4
jwjb1020 <jwjb1020@gmail.com>
2023-05-03 14:04:11 +0900

    2023.05.03

---
+++ Study4/TestClass.java
@@ -0,0 +1,35 @@
+package Study4;
+
+public class TestClass extends Unit2 implements Unit, Unit3 {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void walk() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void die() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nod() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
